1f4ca8a6873802b58dc9d42ade3e445df91270bb
Emese Revfy <re.emese@gmail.com>
2016-06-27 20:43:24 +0200

    release

--- initify_plugin.c
+++ initify_plugin.c
@@ -38,7 +38,7 @@
 int plugin_is_GPL_compatible;
 
 static struct plugin_info initify_plugin_info = {
-	.version	=	"20160527",
+	.version	=	"20160627",
 	.help		=	"disable\tturn off the initify plugin\n"
 				 "verbose\tprint all initified strings and all"
 				 " functions which should be __init/__exit\n"
